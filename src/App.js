8a662b94a49250c7ab08755d3bd940796252c44c
Uneku Ogbaje <unekuogbaje@gmail.com>
2022-09-29 10:31:19 +0100

    layout

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
-
-
 const App = () => {
 
   const offers = [
     {
       id: 1,
-      title: 'move-in & deep cleaning',
-    }
+      title: "Move-in or deep Cleaning",
+    },
+    {
+      id: 2,
+      title: "Regular home Cleaning",
+    },
+    {
+      id: 3,
+      title: "Funiture or upholestery Cleaning",
+    },
+    {
+      id: 4,
+      title: "Laundry",
+    },
+    {
+      id: 5,
+      title: "Fumigation",
+    },
   ]
   return (
     <div className="offers-section">
-      {offers.map((title) => (
+      {offers.map(({title}) => (
         <div className="offer-section">
+        <div className="background-image" />
         <div className="offer-details-section">
           <h2>{title}</h2>
           <p>Book now</p>
