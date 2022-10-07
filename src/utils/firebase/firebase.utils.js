b334597505a8a9f51c23dbbdbc900b64208bb446
Flo <unekuogbaje@gmail.com>
2022-10-07 09:33:49 +0100

    auth

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -22,3 +22,6 @@ const firebaseConfig = {
   provider.selectCustomParameter({
     prompt: "select_account"
   });
+
+  export const auth = getAuth();
+  export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
