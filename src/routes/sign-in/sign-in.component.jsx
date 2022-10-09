4f465f96a55aea56e90fa0b1cb84c17cf809ffa5
Flo <unekuogbaje@gmail.com>
2022-10-09 16:33:41 +0100

    libraries

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -4,7 +4,7 @@ const SignIn = () => {
 
     const logGoogleUser = async () => {
         const { user } = await signInWithGooglePopup();
-        createUserDocumentFromAuth(user);
+        const userDocRef = await createUserDocumentFromAuth(user);
     };
 
     return (
