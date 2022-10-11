68662a430d25bf1f65cce4e5898fbc152a1e0b62
Flo <unekuogbaje@gmail.com>
2022-10-11 15:31:04 +0100

    updated signup functionality

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -1,19 +1,32 @@
+import { useState } from "react";
+
+const defaultFormFields = {
+    displayName: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+}
+
 const SignUp = () => {
+    const [formFields, setFormFields] = useState(defaultFormFields)
+    const { displayName, email, password, confirmPassword } = formFields;
+
+    const handleChange = (event) => {};
     return (
         <div>
             <h1>Sign up with your email & password</h1>
             <form onSubmit={() => {}}>
                 <label>Display Name</label>
-                <input type='text' required/>
+                <input type='text' required onChange={handleChange} />
 
                 <label>Email</label>
-                <input type='email' required/>
+                <input type='email' required onChange={handleChange} />
 
                 <label>Password</label>
-                <input type='password' required/>
+                <input type='password' required onChange={handleChange} />
 
                 <label>Confirm Password</label>
-                <input type='password' required/>
+                <input type='password' required onChange={handleChange} />
                 <button type='submit'>Sign Up</button>
             </form>
         </div>
